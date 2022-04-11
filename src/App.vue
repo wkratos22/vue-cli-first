67d61178afaeabaa8b44341eab4f12c5ebd95db2
wkratos22 <michaelwachnicki2001@gmail.com>
2022-04-11 15:39:38 +0200

    esercizio finito

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <Title_1 msg="La mia prima app con Vue CLI!"/>
+    <img id="img" alt="Vue logo" src="./assets/marc-kleen-h8UQV31X5AI-unsplash.jpg">
+  </div>
+</template>
+
+<script>
+import Title_1 from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Title_1
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+#img{
+  width: 100%;
+  height: 100%;
+}
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1> 
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Title_1',
+  props: {
+    msg: String
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+</style>
